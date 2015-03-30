0a22ce3af5e06c6f2937013fb492d17aeeb607e0
blomster1 <yevic.tabita@gmail.com>
2015-03-30 19:50:57 +0200

    Ajout des variables (pb pour les unique)

---
+++ src/projet/m2104/Adherent.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.m2104;
+
+/**
+ *
+ * @author yevic
+ */
+public class Adherent extends Personne {
+    private int numAdherent;
+    private String adresse;
+    private double soldeNuites = 0;
+    
+}
---
+++ src/projet/m2104/Sortie.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.m2104;
+
+// Importation de l'utilitaire date
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author yevic
+ */
+public class Sortie {
+    private String nomSortie;
+    private GregorianCalendar date;
+    private double prixNuite;
+}
