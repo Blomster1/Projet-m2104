b71e63d9e99247051f4e823b500af27cd8b93be5
blomster <blomster@Blomster>
2015-04-01 23:15:43 +0200

    Mutateurs pour sortie et adherents : OK

--- src/ProjetFFCAM/CAF.java
+++ src/ProjetFFCAM/CAF.java
@@ -8,68 +8,95 @@ import java.io.Serializable;
 
 public class CAF implements java.io.Serializable {
 	
-	private String nomCAF;
-	private String adresse;
-	private String site;
-        private HashSet<Activite> ActivitesDuClub;
-        
+    private String nomCAF;
+    private String adresse;
+    private String site;
+    private HashSet<Activite> activitesDuClub;
+    private HashMap<String, Sortie> sorties;
+    private HashMap<String, Adherent> adherents;
 
-	//Constructeur
-	public CAF(String n, String a, String s) {
-		this.setNomCAF(n);
-		this.setAdresse(a);
-		this.setSite(s);
-                ActivitesDuClub = new HashSet<>();
-	}
+    //Constructeur
+    public CAF(String n, String a, String s) {
+            this.setNomCAF(n);
+            this.setAdresse(a);
+            this.setSite(s);
+            activitesDuClub = new HashSet<>();
+            sorties = new HashMap<>();
+    }
 
-	private void setSite(String s) {
-		site = s;		
-	}
+    //GETTERS
+    public String getSite() {
+            return site;		
+    }
+    public String getNomCAF(){
+            return nomCAF ;
+    }
+    public String getAdresse(){
+            return adresse;
+    }
+    public HashSet<Activite> getActivites() {
+        return activitesDuClub;
+    }
+    public HashMap<String, Sortie> getSorties() {
+        return sorties;
+    }
+    public HashMap<String, Adherent> getAdherents() {
+        return adherents;
+    }
+    public Sortie getSortie(String nomS){
+        //Retourne une sortie en fonction de son nom
+        return getSorties().get(nomS);
+    }
+    public Adherent getAdherent(String nomA){
+        //Retourne un adherent en fonction de son nom
+        return getAdherents().get(nomA);
+    }
         
-	private void setNomCAF(String n){
-		nomCAF = n;
-	}
- 
-	private void setAdresse(String a){
-		adresse = a ;
-	}
-        
-	public String getSite() {
-		return site;		
-	}
-        
+    //SETTERS
+    private void setSite(String s) {
+            site = s;		
+    }
+    private void setNomCAF(String n){
+            nomCAF = n;
+    }
+    private void setAdresse(String a){
+            adresse = a ;
+    }
+    private void setActivites(HashSet<Activite> ActiviteDuClub) {
+        this.activitesDuClub = ActiviteDuClub;
+    }
 
-	public String getNomCAF(){
-		return nomCAF ;
-	}
- 
-	public String getAdresse(){
-		return adresse;
-	}
-
-        public HashSet<Activite> getActivites() {
-            return ActivitesDuClub;
+    private void setSorties(HashMap<String, Sortie> sorties) {
+        this.sorties = sorties;
+    }
+    public void setAdherents(HashMap<String, Adherent> adherents) {
+        this.adherents = adherents;
+    }
+    
+    
+    //AJOUTER ACTIVITE
+    public boolean ajouterActivite(Activite a){
+        if(!this.getActivites().contains(a)){
+            addActivite(a);
+            a.ajouterClub(this);
+            return true;
+        } else {
+            System.out.println("Cette Acivite existe deja pour ce CAF local.");
+            return false;
         }
-
-        private void setActivites(HashSet<Activite> ActiviteDuClub) {
-            this.ActivitesDuClub = ActiviteDuClub;
-        }
-	
-        public boolean ajouterActivite(Activite a){
-            if(!this.getActivites().contains(a)){
-                addActivite(a);
-                a.ajouterClub(this);
-                return true;
-            } else {
-                System.out.println("Cette Acivite existe deja pour ce CAF local.");
-                return false;
-            }
-        }
+    }
 
     private void addActivite(Activite a) {
         this.getActivites().add(a);
     }
-        
-        
-        
+
+   //AJOUTER ADHERENT
+   private void addAdherent(String numa, Adherent a){
+       adherents.put(numa, a);
+   }
+   
+   //AJOUTER SORTIE
+   private void addSortie(String noms, Sortie s){
+       sorties.put(noms, s);
+   }
 }
